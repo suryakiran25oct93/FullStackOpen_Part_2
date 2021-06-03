83bedad2a8813a419eb95a741c9091ccc124f4f9
SURYA KIRAN JONNALAGADDA <46839969+suryakiran25oct93@users.noreply.github.com>
2021-06-03 11:10:50 +0530

    Add files via upload

---
+++ phonebook/phonebookapp/src/components/Person.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Person = ({ person, handleDeletePersonButton }) => (
+  <div>
+    <strong>{person.name}</strong> {person.number} ---{' '}
+    <button
+      type="button"
+      onClick={() => handleDeletePersonButton(person.id, person.name)}
+    >
+      <strong>Delete</strong>
+    </button>
+  </div>
+);
+
+Person.propTypes = {
+  person: PropTypes.objectOf(PropTypes.any).isRequired,
+  handleDeletePersonButton: PropTypes.func.isRequired
+};
+
+export default Person;
